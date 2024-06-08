7b65ac7d0b27e23d404d9170c13d5c765752cf76
Melo <melojulia.ti@gmail.com>
2024-06-08 17:49:14 -0300

    Primeiro commit

---
+++ BancoDigital/src/Aplicação/Main.java
@@ -0,0 +1,22 @@
+package Aplicação;
+
+import Entidades.Cliente;
+import Entidades.Conta;
+import Entidades.ContaCorrente;
+import Entidades.ContaPoupanca;
+
+public class Main {
+    public static void main(String[] args) {
+		Cliente venilton = new Cliente();
+		venilton.setNome("Venilton");
+		
+		Conta cc = new ContaCorrente(venilton);
+		Conta poupanca = new ContaPoupanca(venilton);
+
+		cc.depositar(100);
+		cc.transferir(100, poupanca);
+		
+		cc.imprimirExtrato();
+		poupanca.imprimirExtrato();
+	}
+}
